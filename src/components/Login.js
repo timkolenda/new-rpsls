e8e300df209167ec4fa1995b8fe2ed6dc0ee853d
Tim <tkolenda@gmail.com>
2019-01-25 14:54:26 -0500

    working on setting up swal

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,10 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Link, Redirect } from "react-router-dom";
 import SweetAlert from 'sweetalert-react';
+import swal from '@sweetalert/with-react';
+
+// import 'sweetalert/dist/sweetalert.css';
+// import 'node_modules/sweetalert/src/sweetalert.scss';
 
 import Button from './Button';
 
@@ -12,35 +16,55 @@ class Login extends Component {
     }
 
     handleSubmit = (e) => {
-        this.props.handleNewPlayerFormSubmit(e)
-        this.setState({ toGame: true });
+        if (this.props.playerNameReady) {
+            this.props.handleNewPlayerFormSubmit(e)
+            this.setState({ toGame: true });
+        } else {
+            this.displayInvalidPlayerNameMessage();
+        }
     }
 
-    testFunction = () => console.log(this.props.playerNameReady);
+    handleClick = () => {
+        if (this.props.playerNameReady) {
+            this.props.addNewPlayerToFirebase();
+        } else {
+            this.displayInvalidPlayerNameMessage();
+        }
+    }
 
-    
+    displayInvalidPlayerNameMessage = () => {
+        this.setState({ show: true });
+    }
 
-    render() {
-        this.testFunction();
+
+
+    render() {  
         if (this.state.toGame === true){
             return <Redirect to={'/game'} />
         }
         
         return (
             <div className="Login">
+                <SweetAlert
+                    show={this.state.show}
+                    title="Demo"
+                    text={'hello world'}
+                    onConfirm={() => this.setState({ show: false })}
+                />
                 <h2>RPSLS</h2>
                 <form className="playerNameSubmissionForm" action="#"  onSubmit={this.handleSubmit} >
                     <label className="visuallyhidden" htmlFor="playerName">Please enter your nickname!</label>
                     <input className="playerNameSubmissionForm__input" type="text" id="playerName" value={this.props.playerName} onChange={(e) => this.props.handleChange(e, this.props.checkForPlayerNameReady)} />
                 </form>
                 <div className="loginPageButtons">
-                    <div className="loginPageButtons__buttonContainer" onClick={this.props.addNewPlayerToFirebase}>
-                        <Button destination={this.state.playerNameReady ? 'game' : '/'} message='Play Game'/>
+                    <div className="loginPageButtons__buttonContainer" onClick={this.handleClick}>
+                        <Button destination={this.props.playerNameReady ? 'game' : '/'} message='Play Game'/>
                     </div>
                     <div className="loginPageButtons__buttonContainer">
                         <Button destination='rules' />
                     </div>
                 </div>
+                
             </div>
         );
     }
