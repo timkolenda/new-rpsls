d99f93f4aa1b11ea4f28486e645685e4f733605f
Tim <tkolenda@gmail.com>
2019-04-19 16:26:47 -0400

    Updates

---
+++ precache-manifest.ddee83deed67b029c5ada43e0c535f87.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "10fdffee8eb32d33d8fe",
+    "url": "/timkolenda/new-rpsls.git/static/css/main.a9a0afe0.chunk.css"
+  },
+  {
+    "revision": "10fdffee8eb32d33d8fe",
+    "url": "/timkolenda/new-rpsls.git/static/js/main.10fdffee.chunk.js"
+  },
+  {
+    "revision": "eb4d2b182ed05ab3c25c",
+    "url": "/timkolenda/new-rpsls.git/static/js/1.eb4d2b18.chunk.js"
+  },
+  {
+    "revision": "5a01284567a467d97056",
+    "url": "/timkolenda/new-rpsls.git/static/js/runtime~main.5a012845.js"
+  },
+  {
+    "revision": "ce8f19bd627e77ba0995574f91143385",
+    "url": "/timkolenda/new-rpsls.git/static/media/background.ce8f19bd.png"
+  },
+  {
+    "revision": "e4fa02f8fe23b9871e61a119b7e6083b",
+    "url": "/timkolenda/new-rpsls.git/index.html"
+  }
+];
--- src/api/unsplash.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-
-export default axios.create({
-    baseURL: 'https://api.unsplash.com/',
-    headers: {
-        Authorization: 'Client-ID 630c588d9c659156e3aeb6db866809578dadb2520e92e3eaaa7d6c58a069053a'
-    }
-});
